ed255e8a6b66576ea0d8c476767b96fb4e3e4824
knightbane186 <denverlopes93@gmail.com>
2024-07-02 00:09:23 +1000

    setup before native

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function App() {
     return (
         <View style={styles.container}>
+
             <Text>Aora!</Text>
             <StatusBar style="auto" />
+            <Link href = "profile">Got to profile </Link>
         </View>
     );
 }
