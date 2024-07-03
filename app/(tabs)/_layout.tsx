50c9ad7183932d0ec74f6c6c32609bbd214cb501
knightbane186 <denverlopes93@gmail.com>
2024-07-04 01:03:01 +1000

    working on home page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,39 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Image } from 'react-native';
+import React from 'react';
+import { Tabs } from 'expo-router';
+import { icons } from '../../constants';
+
+const TabIcon = ({ icon, color, name, focused }) => {
+  return (
+    <View>
+      <Image
+        source={icon}
+        style={{ tintColor: color, width: 24, height: 24 }}
+      />
+    </View>
+  );
+};
 
 const TabsLayout = () => {
   return (
-    <View>
-      <Text> TabsLayout</Text>
-    </View>
-  )
-}
+    <Tabs>
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: 'Home',
+          headerShown: false,
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              icon={icons.home}
+              color={color}
+              name="Home"
+              focused={focused}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
 
-export default TabsLayout
+export default TabsLayout;
--- app/index.tsx
+++ app/index.tsx
@@ -7,7 +7,7 @@ export default function App() {
         <View className="flex-1 items-center justify-center bg-white">
             <Text className = "text-3xl font-pblack">Aora!</Text>
             <StatusBar style="auto" />
-            <Link href = "profile">Got to profile </Link>
+            <Link href = "/home">Got to home </Link>
         </View>
     );
 }
