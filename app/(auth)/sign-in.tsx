92bb34522dd21cd96ad485b28e5d72dd5acb8268
knightbane186 <denverlopes93@gmail.com>
2024-07-06 00:32:56 +1000

    adding basics for auth

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,9 +1,16 @@
 import { View, Text, ScrollView, Image } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../../constants';
+import FormField from '@/components/FormField';
 
 const SignIn = () => {
+
+    const [form,setForm] =useState({
+       email: '',
+        password:''
+    })
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
@@ -16,6 +23,25 @@ const SignIn = () => {
           <Text className="text-2xl text-white font-semibold mt-10 font-psemibold">
             Log in to Aora
           </Text>
+          <FormField
+          title="Email"
+          value={form.email}
+          handleChangeText={(e) => setForm({
+...form,
+email:e
+          })}
+          otherStyles="mt-7"
+          keyBoardType="email-address"
+          />
+                    <FormField
+          title="Password"
+          value={form.password}
+          handleChangeText={(e) => setForm({
+...form,
+password:e
+          })}
+          otherStyles="mt-7"
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
