497e5d25eee0b0189ca1760e8e8412dfb7b2a6af
knightbane186 <denverlopes93@gmail.com>
2024-07-05 22:50:44 +1000

    adding auth

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,19 +1,25 @@
-import { View, Text, ScrollView  } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { View, Text, ScrollView, Image } from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../../constants';
 
-
-const signIn = () => {
+const SignIn = () => {
   return (
-<SafeAreaView className=" bg-primary h-full" >
-<ScrollView>
-<View className="w-full justify-center min-h-[85vh] px-4 my-6">
-    
-
-</View>
-</ScrollView>
-</SafeAreaView>
-  )
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center h-full px-4 my-6">
+          <Image 
+            source={images.logo}
+            resizeMode='contain' // Fixed the case for resizeMode
+            className="w-[115px] h-[35px]"
+          />
+          <Text className="text-2xl text-white font-semibold mt-10 font-psemibold">
+            Log in to Aora
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
 
-export default signIn
+export default SignIn;
